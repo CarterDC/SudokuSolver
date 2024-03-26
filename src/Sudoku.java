335aad4ba2d1e823822628d2f2b2f7b0b233850b
Romain COIRIER <coirier.rom1@gmail.com>
2024-03-26 07:50:03 +0100

    javadoc + refactor Arguments

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,33 +1,46 @@
-
+// TODO : faire une grille sans doublon mais qui ne soit pas faisable
 import utils.*;
 
 import java.util.Optional;
-
+/**
+ * Classe principale (Main) du programme de résolution de grilles Sudoku
+ * <p>
+ * Projet développé en java, dans le cadre de l'UE NFP136
+ * usage : {@code java Sudoku <fileName> [<nbSolutions>]}
+ * @author Romain COIRIER
+ */
 public class Sudoku {
+   
+    /**
+     * Point d'entrée du programme
+     * <p>
+     * Gère le déroulement du programme : récupération des arguments,
+     * vérification des données, solutionnement de la grille proposée et enfin affichage des résultats.
+     * @param args
+     */
     public static void main(String[] args) {
 
         Arguments myArgs = Arguments.parseArguments(args);
-        if(myArgs.displayHelp) {displayHelp(); return;}
+        if(myArgs.shouldDisplayHelp()) {displayHelp(); return;}
 
-        //TODO : Comment here
-        Optional<int[][][]> cellArray = Grid.parseGridFile(myArgs.fileName);
+        // Transforme si possible le fichier fourni en tableau d'int @see #utils.Grid
+        Optional<int[][][]> cellArray = Grid.parseGridFile(myArgs.getFileName());
         if (cellArray.isEmpty()) { return; }
-
-        System.out.println("Le fichier fourni contient bien une grille de 9x9 valeurs valides.");   
         // a ce point on sait qu'on a bien 9 lignes de 9 cases, composées exclusivement des 9 chiffres
         // reste a vérifier l'absence de doublons sur chaque ligne, chaque colonne et chaque carré.
+        System.out.println("Le fichier fourni contient bien une grille de 9x9 valeurs valides.");  
 
-        if (Grid.hasDuplicates(cellArray.get())) { return; }        
-        System.out.println("La grille fournie ne contient aucun doublons.");
+        if (Grid.hasDuplicates(cellArray.get())) { return; }
         // à ce point on sait que la grille est valide
         // reste a vérifier si elle a bien des solutions
-        
+        System.out.println("La grille fournie ne contient aucun doublons.");
+
         //affichage de la grille d'origine pour référence
         Grid.displayGrid(cellArray.get());
         
         // Résolution :
         // Effectue plusieurs passes successives pour tenter de trouver le nombre de solutions passé en paramètre
-        SolveResult result = Grid.solve(cellArray.get(), myArgs.nbMaxSolutions);
+        SolveResult result = Grid.solve(cellArray.get(), myArgs.getNbMaxSolutions());
         // Affichage des résultats
         result.displayGrids();
         result.displayStats();
@@ -45,6 +58,8 @@ public class Sudoku {
 
         System.out.println(helpString);
     }
+
+    private Sudoku(){}
 
 }
 
--- src/utils/Arguments.java
+++ src/utils/Arguments.java
@@ -2,22 +2,43 @@ package utils;
 
 import java.text.MessageFormat;
 
+/**
+ * Cette classe gère les arguments passés à la fonction main.
+ * <p>
+ * C'est donc aussi elle qui défini la valeur par défault des arguments optionnels
+ */
 public class Arguments {
+
+        /**
+         * Valeur par default du nb maximum de solutions recherchées
+         */
         public static final int MAX_SOLUTIONS_DEFAULT = 2;
 
-        public String fileName;
-        public int nbMaxSolutions;
-        public boolean displayHelp = false;
+        private String fileName; 
+        private int nbMaxSolutions;
+        private boolean shouldDisplayHelp = false;
 
+        /**
+         * Méthode de classe qui parse le tableau de String[] args passé à la function main
+         * <p>
+         * 
+         * @param args
+         * @return - Une instance de la classe Argument contenant le résultat du parsing
+         */
         public static Arguments parseArguments(String [] args){
             Arguments myArgs = new Arguments();
             
+            //on prévoit d'afficher l'aide si l'utilisateur le demande ou s'il n'a passé aucun argument du tout
             if(args.length == 0 || args[0].equals("-h") || args[0].equals("--help")) {
-                myArgs.displayHelp = true;
+                myArgs.shouldDisplayHelp = true;
                 return myArgs;
             } else {
+                // le premier argument obligatoire est le nom de fichier
+                // on laisse la suite du programme vérifier si c'est un nom de fichier valide ou pas
                 myArgs.fileName = args[0];
                 if(args.length > 1) {
+                    // l'utilisateur a passé un ou plusieur paramètres optionnels
+                    // on ne s'occupe que du premier, qui doit être un int (nbMaxSoluces)
                     try{
                         myArgs.nbMaxSolutions = Integer.parseInt(args[1]);    
                     } catch(NumberFormatException e) {
@@ -31,9 +52,36 @@ public class Arguments {
             return myArgs;
         } 
 
+        /**
+         * Constructeur avec les valeurs par défault
+         */
         public Arguments(){
             this.fileName = "";
             this.nbMaxSolutions = Arguments.MAX_SOLUTIONS_DEFAULT;
+        }
+
+        /**
+         * Getter de la variable privée fileName
+         * @return - String : un nom de fichier valide ou pas
+         */
+        public String getFileName() {
+            return this.fileName;
+        }
+
+        /**
+         * Getter de la variable privée nbMaxSolutions
+         * @return - int : le nombre de soluces souhaitées
+         */
+        public int getNbMaxSolutions() {
+            return this.nbMaxSolutions;
+        }
+
+        /**
+         * Getter de la variable privée shoudlDisplayHelp
+         * @return - boolean : est ce qu'il faut afficher l'aide ou pas
+         */
+        public boolean shouldDisplayHelp() {
+            return this.shouldDisplayHelp;
         }
     
 }
